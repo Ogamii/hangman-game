53f7428c1cb1ab51c742c5497c8d2c45654de09d
Breiner Jimenez <breiner1803@gmail.com>
2022-08-05 20:39:58 -0500

    Eleccion de palabra al azar desde un archivo data.txt

--- hangman_game.py
+++ hangman_game.py
@@ -1,26 +1,23 @@
 import random
 
 
-# def word_choice():
-#     data = []
-#     with open("./archivos/data.txt","r",encoding="utf-8") as f:
-#         for line in f:
-#             line = f.readline()
-#             line = line.strip("\n")
-#             data.append(line)
-#         print(data)
-#         print("\n")
-
-
 def word_choice():
-    data = []
+    keys = []
+    values = []
     with open("./archivos/data.txt","r",encoding="utf-8") as f:
-        for line in f:
-            line = f.readline()
-            line = line.strip("\n")
-            data.append(line)
-        word_list = list(map(lambda word: word,data))
+        values = [line.replace("\n","") for line in f]
+        num_words = len(values)
+        keys = [i for i in range(1, num_words+1)]
+    word_list = {keys[i]: values[i] for i in range(0, num_words) }
+    random_key = random.randint(0,num_words-1)
+    random_word =word_list[random_key]
+    return random_word
+    
+
+
+def run():
+    print(word_choice())
 
 
 if __name__ == '__main__':
-    word_choice()
+    run()
